cca3276233c6e59c0660bf77a48ca29cb78db5b6
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-06-19 17:28:54 -0300

    chore: config styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app';
 
+import GloablStyle from 'styles/global';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GloablStyle />
+    </>
+  );
 }
 
 export default MyApp;
