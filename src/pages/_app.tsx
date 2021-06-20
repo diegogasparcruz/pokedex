abcf401d181caa2873121de8aaf241399c18ed5b
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-06-20 00:29:09 -0300

    chore: config theme styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 
 import GloablStyle from 'styles/global';
+import theme from 'styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <GloablStyle />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,4 +6,47 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.white};
+    font: ${({ theme }) =>
+      `${theme.fonts.weight.regular} ${theme.fonts.size.sm} ${theme.fonts.family}`};
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: ${({ theme }) => theme.fonts.weight.medium};
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.primary};
+    border-radius: 30px;
+  }
+
+  ::-webkit-scrollbar {
+    width: 0.25rem;
+  }
 `;
