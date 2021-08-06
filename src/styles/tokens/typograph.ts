44132ce745f6c800090e61259c51dff184cac857
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-08-06 02:56:08 -0300

    style: add colors and typograph tokens

---
+++ src/styles/tokens/typograph.ts
@@ -0,0 +1,14 @@
+export const typograph = {
+  sizes: {
+    xs: '.75rem',
+    sm: '1rem',
+    md: '1.625rem',
+    lg: '2rem',
+    xl: '6.25rem',
+  },
+  weight: {
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+} as const;
