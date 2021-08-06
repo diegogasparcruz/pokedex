d8636a9a593012c8d517979f23a3e1876aeea46b
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-08-06 03:26:27 -0300

    chore: change imports icons

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,66 @@
+import { ReactComponent as Back } from './back.svg';
+import { ReactComponent as Filter } from './filter.svg';
+import { ReactComponent as Front } from './front.svg';
+import { ReactComponent as Generation } from './generation.svg';
+import { ReactComponent as Search } from './search.svg';
+import { ReactComponent as Sort } from './sort.svg';
+
+import { ReactComponent as Medium } from './heights/medium.svg';
+import { ReactComponent as Short } from './heights/short.svg';
+import { ReactComponent as Tall } from './heights/tall.svg';
+
+import { ReactComponent as Heavy } from './weights/heavy.svg';
+import { ReactComponent as Light } from './weights/light.svg';
+import { ReactComponent as NormalWeight } from './weights/normal.svg';
+
+import { ReactComponent as Bug } from './types/bug.svg';
+import { ReactComponent as Dark } from './types/dark.svg';
+import { ReactComponent as Dragon } from './types/dragon.svg';
+import { ReactComponent as Electric } from './types/electric.svg';
+import { ReactComponent as Fairy } from './types/fairy.svg';
+import { ReactComponent as Fighting } from './types/fighting.svg';
+import { ReactComponent as Fire } from './types/fire.svg';
+import { ReactComponent as Flying } from './types/flying.svg';
+import { ReactComponent as Ghost } from './types/ghost.svg';
+import { ReactComponent as Grass } from './types/grass.svg';
+import { ReactComponent as Ground } from './types/ground.svg';
+import { ReactComponent as Ice } from './types/ice.svg';
+import { ReactComponent as Normal } from './types/normal.svg';
+import { ReactComponent as Poison } from './types/poison.svg';
+import { ReactComponent as Psychic } from './types/psychic.svg';
+import { ReactComponent as Rock } from './types/rock.svg';
+import { ReactComponent as Steel } from './types/steel.svg';
+import { ReactComponent as Water } from './types/water.svg';
+
+export const icons = {
+  back: <Back />,
+  filter: <Filter />,
+  front: <Front />,
+  generation: <Generation />,
+  search: <Search />,
+  sort: <Sort />,
+  medium: <Medium />,
+  short: <Short />,
+  tall: <Tall />,
+  heavy: <Heavy />,
+  light: <Light />,
+  normalWeight: <NormalWeight />,
+  bug: <Bug />,
+  dark: <Dark />,
+  dragon: <Dragon />,
+  electric: <Electric />,
+  fairy: <Fairy />,
+  fighting: <Fighting />,
+  fire: <Fire />,
+  flying: <Flying />,
+  ghost: <Ghost />,
+  grass: <Grass />,
+  ground: <Ground />,
+  ice: <Ice />,
+  normal: <Normal />,
+  poison: <Poison />,
+  psychic: <Psychic />,
+  rock: <Rock />,
+  steel: <Steel />,
+  water: <Water />,
+};
